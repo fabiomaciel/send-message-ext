a948934e67395799aa6b0ed1be611bb72e9f7189
Magno Gouveia <owlflee@gmail.com>
2017-06-20 20:43:24 -0300

    a porra toda

--- popup.js
+++ popup.js
@@ -1,1 +1,25 @@
-console.log('oi');
+document.addEventListener('DOMContentLoaded',()=>{
+    const button = document.getElementById("button"),
+          text = document.getElementById("insert"),
+          textArea = document.getElementById("textarea");
+    
+    button.addEventListener("click",()=>{
+        let form = {"command": text.value}
+        let resp = null;
+        let opts = {
+            method: 'POST',
+            body: JSON.stringify(form),
+            headers: {}
+        };
+        fetch('http://10.7.30.109:4545', opts)
+            .then((response) => {
+                return response.json();
+            })
+            .then((body)=>{
+                textArea.value = body;
+            })
+            .catch((e)=>{
+                console.log(e);
+            });
+    })
+});
