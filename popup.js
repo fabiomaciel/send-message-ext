732e7684fa47f63b83a578eb822a673fd3ca3911
Fabio Maciel <fabio.souza@metasix.com.br>
2018-10-18 08:11:35 -0300

    added new features and users

--- popup.js
+++ popup.js
@@ -1,43 +1,86 @@
-const serverHost = "http://10.7.30.109:4545";
-const serverRoot = "http://10.7.30.109:4546";
+"use strict";
+
+const server = {
+  host: "124",
+  get hostURL() {
+    return `http://10.7.30.${this.host}:4545`;
+  },
+
+  get rootURL() {
+    return `http://10.7.30.${this.host}:4546`;
+  },
+};
+
+function setHost() {
+  server.host = document.querySelector("input[name='host']:checked").value;
+}
 
 document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll("input[name='host']").forEach(el =>
+    el.addEventListener("click", event => {
+      setHost();
+    }),
+  );
+
   const button = document.getElementById("button"),
     text = document.getElementById("insert"),
     textArea = document.getElementById("textarea");
 
   button.addEventListener("click", () => {
     let form = { command: text.value };
-    let resp = null;
-    execute(form);
+    execute(text.value);
   });
 
   const graphicButton = document.getElementById("sendGraphic"),
     graphicText = document.getElementById("insertGraphic");
 
   graphicButton.addEventListener("click", () => {
-    let form = { command: `DISPLAY=:0.0 ${graphicText.value}` };
-    let resp = null;
-
-    execute(form);
+    execute(`DISPLAY=:0.0 ${graphicText.value}`);
   });
 
   const rootButton = document.getElementById("sendRoot"),
     rootText = document.getElementById("insertRoot");
 
   rootButton.addEventListener("click", () => {
-    let form = { command: `${rootText.value}` };
-    execute(form, serverRoot);
+    execute(rootText.value, server.rootURL);
   });
 
-  function execute(body, server) {
+  document.getElementById("desbloquear").addEventListener("click", () => {
+    execute("loginctl unlock-sessions", server.rootURL);
+  });
+
+  const xvideosButton = document.getElementById("sendXvideos"),
+    xvideosText = document.getElementById("xvideos");
+
+  xvideosButton.addEventListener("click", () => {
+    execute(
+      `DISPLAY=:0.0 google-chrome xvideos.com/?k=${encodeURI(
+        xvideosText.value,
+      )}`,
+    );
+  });
+
+  const chromeButton = document.getElementById("sendChrome"),
+    chromeText = document.getElementById("chrome");
+
+  chromeButton.addEventListener("click", () => {
+    execute(`DISPLAY=:0.0 google-chrome ${encodeURI(chromeText.value)}`);
+  });
+
+  function execute(command, serverURL) {
+    let body = { command };
+    serverURL = serverURL || server.hostURL;
     let opts = {
       method: "POST",
       body: JSON.stringify(body),
       headers: {},
     };
-  
-    return fetch( server || serverHost, opts)
+
+    console.log(
+      `trying to reach host ${serverURL} with data ${JSON.stringify(opts)}`,
+    );
+
+    return fetch(serverURL, opts)
       .then(response => {
         return response.text();
       })
@@ -48,7 +91,4 @@ document.addEventListener("DOMContentLoaded", () => {
         console.log(e);
       });
   }
-
 });
-
-
